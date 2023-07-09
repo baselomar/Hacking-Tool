8bd64245501ff95bc2fada88c575af3c592abbc0
baselomar <125217474+baselomar@users.noreply.github.com>
2023-07-10 02:32:42 +0300

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -10,7 +10,7 @@ MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\0
 REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
 MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
 RESETBG="$(printf '\e[0m\n')"
-
+menu(){
 printf "                          ${RED}  .:.  Version 1.0 Beta  .:.   \e[0m\n"${RESETBG}
 printf "\n"
 printf "   \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Tool Created by Basel Omar \e[0m\n"
@@ -23,7 +23,7 @@ printf "\n"
 printf "\n"
 read -p $'\e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select an option: \e[0m\e[1;96m\e' menu_option
 
-
+}
 
 if [[ $menu_option == 1 || $menu_option == 01 ]]; then
 instagram
