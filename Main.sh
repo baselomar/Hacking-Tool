a94e2d946a4e128f1678c2a2544a163603ae9637
baselomar <125217474+baselomar@users.noreply.github.com>
2023-07-10 02:18:26 +0300

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -10,17 +10,7 @@ MAGENTA="$(printf '\033[35m')"  CYAN="$(printf '\033[36m')"  WHITE="$(printf '\0
 REDBG="$(printf '\033[41m')"  GREENBG="$(printf '\033[42m')"  ORANGEBG="$(printf '\033[43m')"  BLUEBG="$(printf '\033[44m')"
 MAGENTABG="$(printf '\033[45m')"  CYANBG="$(printf '\033[46m')"  WHITEBG="$(printf '\033[47m')" BLACKBG="$(printf '\033[40m')"
 RESETBG="$(printf '\e[0m\n')"
-banner() {
-echo '
-          ██████╗      █████╗     ███████╗    ███████╗    ██╗               
-▄ ██╗▄    ██╔══██╗    ██╔══██╗    ██╔════╝    ██╔════╝    ██║         ▄ ██╗▄
- ████╗    ██████╔╝    ███████║    ███████╗    █████╗      ██║          ████╗
-▀╚██╔▀    ██╔══██╗    ██╔══██║    ╚════██║    ██╔══╝      ██║         ▀╚██╔▀
-  ╚═╝     ██████╔╝    ██║  ██║    ███████║    ███████╗    ███████╗      ╚═╝ 
-          ╚═════╝     ╚═╝  ╚═╝    ╚══════╝    ╚══════╝    ╚══════╝          
-  '    |lolcat                                                                      
 
-}
 printf "      \e[1;97m  .:.  Version 1.0 Beta  .:.   \e[0m\n"
 printf "\n"
 printf "   \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Tool Created by Basel Omar \e[0m\n"
