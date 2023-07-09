2b47f9646b7d77711644cd371a8b731fbed6e87a
baselomar <125217474+baselomar@users.noreply.github.com>
2023-07-10 02:10:58 +0300

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -2,7 +2,7 @@
 # Basha Virus V1.0
 # Coded by: SiddhantOffl
 # Github: https://github.com/SiddhantOffl
-
+clear
 
 ## ANSI colors (FG & BG)
 RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
