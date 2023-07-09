9c1a52df204e783f9e83f6c2b48fd77c0cc91423
baselomar <125217474+baselomar@users.noreply.github.com>
2023-07-10 02:26:16 +0300

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -15,7 +15,24 @@ printf "                             \e[1;97m  .:.  Version 1.0 Beta  .:.   \e[0
 printf "\n"
 printf "   \e[92m[\e[37;1m+\e[92m]\e[0m\e[33;1m Tool Created by Basel Omar \e[0m\n"
 printf "\n"
-printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;96  Show My Ip And HostName\e[0m      \e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;96m Ip Info   \e[0m          \e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;96m Get Insta User   \e[0m   \n"  
+printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;96  Show My Ip & HN\e[0m      \e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;96m Ip Info   \e[0m          \e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;96m Get Insta User   \e[0m   \n"  
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select an option: \e[0m\e[1;96m\e' menu_option
 
+
+
+if [[ $menu_option == 1 || $menu_option == 01 ]]; then
+instagram
+
+elif [[ $menu_option == 2 || $menu_option == 02 ]]; then
+facebook
+
+elif [[ $menu_option == 3 || $menu_option == 03 ]]; then
+server="snapchat"
+start1
+else
+printf "\e[1;93m [!] Invalid option!\e[0m\n"
+menu
+fi
+}
 
 
