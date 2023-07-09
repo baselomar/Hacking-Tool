c3b993ec0901813eba7de4594e17aec4891bc7ba
baselomar <125217474+baselomar@users.noreply.github.com>
2023-07-09 15:43:58 +0300

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -11,7 +11,7 @@ RESETBG="$(printf '\e[0m\n')"
 printf "\n"
 printf "\n"
 printf "\n" 
-
+banner() {
 echo '
 ██████╗  █████╗ ███████╗███████╗██╗     
 ██╔══██╗██╔══██╗██╔════╝██╔════╝██║     
@@ -21,5 +21,32 @@ echo '
 ╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝
                                         
 ' |lolcat
+}
+
+menu() {
+printf "\e[0m\n"
+printf "\e[0m\e[1;31m  [\e[0m\e[1;37m01\e[0m\e[1;31m]\e[0m\e[1;33m My IP\e[0m\n"
+printf "\e[0m\e[1;31m  [\e[0m\e[1;37m02\e[0m\e[1;31m]\e[0m\e[1;33m Track Ip\e[0m\n"
+printf "\e[0m\e[1;31m  [\e[0m\e[1;37m00\e[0m\e[1;31m]\e[0m\e[1;33m Exit\e[0m\n"
+printf "\e[0m\n"
+read -p $'  \e[1;31m[\e[0m\e[1;37m~\e[0m\e[1;31m]\e[0m\e[1;92m Select An Option \e[0m\e[1;96m: \e[0m\e[1;93m\en' option
+if [[ $option == 1 || $option == 01 ]]; then
+myipaddr
+elif [[ $option == 2 || $option == 02 ]]; then
+useripaddr
+elif [[ $option == 0 || $option == 00 ]]; then
+sleep 1
+printf "\e[0m\n"
+printf "\e[0m\n"
+exit 1
+
+else
+printf " \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+
+}
 
 			
