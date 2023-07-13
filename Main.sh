e1a0177bb2ad90d7ca09a8f5667d2e6bb09b6d87
baselomar <125217474+baselomar@users.noreply.github.com>
2023-07-13 04:49:28 +0300

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -1,2 +1,14 @@
 clear
 bash setup.sh
+
+#colors
+RED     = '\033[31m'
+GREEN   = '\033[32m'
+YELLOW  = '\033[33m'
+BLUE    = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN    = '\033[36m'
+WHITE   = '\033[37m'
+RESET   = '\033[39m'
+menu() {
+printf "\033[31m[\033[39m1\033[31m]\033[34m • Facebook \033[39m
